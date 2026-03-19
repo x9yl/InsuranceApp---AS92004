d5e3a55e00dd00cc90186b2e66f4078bf1f54c1a
x9yl <jfancosomali@gmail.com>
2026-03-20 09:09:21 +1300

    Fixed some more errors, for example I was using Global variables wrong, but that problem is mainly fixed now.

--- AS92004/Program.cs
+++ AS92004/Program.cs
@@ -1,58 +1,79 @@
-﻿using System.Net.Http.Headers;
+﻿using System.ComponentModel.Design;
+using System.Net.Http.Headers;
 using System.Security.Cryptography.X509Certificates;
 
 namespace AS92004
 {
     internal class Program
     {
-        //Declaring my Constant Variables for the Program. This includes device prices, insurance rate, category type, monthlyRate
-        
+        //Declaring my Consant Variables, this includes the Monthly insurance rate and the normal device amount insurance rate.
         public static decimal insuranceRate = 0.9m, monthlyRate = 0.95m;
-        public static int laptop = 1, desktop = 2, other = 3;
         //Declaring my Global variables. This includes the list for price, devicetype and the Device amount
-        public static int deviceAmount = 0;
-        List<decimal> price = new List<decimal>();
-        List<string> deviceType = new List<string>();
+        public static List<string> InsuranceSummary = new List<string>();
+       
+       
+        
+        
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(" /$$$$$$                                                                                       /$$$$$$                     \r\n|_  $$_/                                                                                      /$$__  $$                    \r\n  | $$   /$$$$$$$   /$$$$$$$ /$$   /$$  /$$$$$$  /$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$       | $$  \\ $$  /$$$$$$   /$$$$$$ \r\n  | $$  | $$__  $$ /$$_____/| $$  | $$ /$$__  $$|____  $$| $$__  $$ /$$_____/ /$$__  $$      | $$$$$$$$ /$$__  $$ /$$__  $$\r\n  | $$  | $$  \\ $$|  $$$$$$ | $$  | $$| $$  \\__/ /$$$$$$$| $$  \\ $$| $$      | $$$$$$$$      | $$__  $$| $$  \\ $$| $$  \\ $$\r\n  | $$  | $$  | $$ \\____  $$| $$  | $$| $$      /$$__  $$| $$  | $$| $$      | $$_____/      | $$  | $$| $$  | $$| $$  | $$\r\n /$$$$$$| $$  | $$ /$$$$$$$/|  $$$$$$/| $$     |  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$$      | $$  | $$| $$$$$$$/| $$$$$$$/\r\n|______/|__/  |__/|_______/  \\______/ |__/      \\_______/|__/  |__/ \\_______/ \\_______/      |__/  |__/| $$____/ | $$____/ \r\n                                                                                                       | $$      | $$      \r\n                                                                                                       | $$      | $$      \r\n                                                                                                       |__/      |__/      ");
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("!!This program is for educational use only!!");
-          
+            char continueInput = 'y';
+            while (continueInput == 'y' || continueInput.Equals('y'))
+            {
+                OneDevice();
+
+                Console.WriteLine("\n--------------------------------\nDo you want to process another device? (y/n)");
+                continueInput = Console.ReadLine()[0];
+            }
             
+
 
 
 
             //summary
         }
-        public static string OneDevice(int deviceAmount, int deviceType, int laptop, int desktop, int other, decimal price)
+        //for my OneDevice I am limiting it to only user input.
+       public static string OneDevice()
         {
-            //Declaring Local variables for OneDevice
+            //Declaring Local variables for OneDevice, this includes all the variables that will be listed in the output.
             string onedeviceOutput = "";
-            
-            //capture device type
+            string id = "";
+            decimal price = 0;
+            string deviceName = "";
+            int deviceAmount = 0;
+            List<string> deviceCategory = new List<string> {"Laptop", "Desktop", "Other" };
+            id = Guid.NewGuid().ToString();
+
+            //capture device name
             Console.WriteLine("\n\n What is device would you like to calculate? : ");
-            deviceType.Equals(Console.ReadLine());
+            deviceName.Equals(Console.ReadLine());
 
 
             //Capture Device amount
             Console.WriteLine("\nHow many of these are you wanting to calculate? : ");
             deviceAmount = (Convert.ToInt32(Console.ReadLine()));
 
-            //Capture Device type
+            //Capture Device price
             Console.WriteLine("\nHow much did your device cost? : ");
             price = Convert.ToDecimal(Console.ReadLine());
 
 
+
             //Calculate number of devices in each category
-            Console.WriteLine("What category would your device fit under? : \n (1. Laptop \n(2. Desktop \n(3. Other ");
+          
+                Console.WriteLine("\nWhat category would your device fit under? : \n (1. Laptop \n(2. Desktop \n(3. Other ");
+                deviceCategory.
+               
+            
 
-
-
+            onedeviceOutput = $"ID : {id}\nName : {deviceName}\nPrice : {price}\nAmount : {deviceAmount}\nInsured Value : {CalculateValue(deviceAmount, insuranceRate, price)}\nMonthly Depreciation :  ";
             return onedeviceOutput;
+            
         }
-        public static decimal CalculateCost(int deviceAmount, string displaydeviceType, decimal price)
+        public static decimal CalculateCost(int deviceAmount, decimal price)
         {
             decimal cost = 0;
             cost = deviceAmount * price;
@@ -62,28 +83,25 @@ namespace AS92004
         }
 
 
-        public static decimal CalculateValue(int cost, int deviceAmount,decimal insuranceRate, decimal price)
+        public static decimal CalculateValue(int deviceAmount, decimal price)
         {
             decimal valueafterInsured = 0.0m;
 
-           if (deviceAmount >= 6)
+            if (deviceAmount >= 6)
             {
-                valueafterInsured = deviceAmount - 5 * cost * insuranceRate + (5 * cost);
+                valueafterInsured = ((CalculateCost(deviceAmount,price) - (5 * price)) * insuranceRate);
+                valueafterInsured = valueafterInsured + (5 * price);
+
+
             }
+            else
+            {
+                valueafterInsured = CalculateCost(deviceAmount, price);
+            }
+            
+           
             return valueafterInsured;
         }
-        public static decimal CalculateMonthlyValue(decimal valuefterInsured)
-        {
-          
-
-
-
-
-
-        }
-        public static string GenerateOutput()
-        {
-
-        }
+        
     }
 }
