b297d1052f5bf423166e74439b02059fb9e79a76
x9yl <jfancosomali@gmail.com>
2026-03-23 12:01:36 +1300

    Finished the Summary

--- AS92004/Program.cs
+++ AS92004/Program.cs
@@ -7,37 +7,44 @@ namespace AS92004
 {
     internal class Program
     {
-        //Declaring my Consant Variables, this includes the Monthly insurance rate and the normal device amount insurance rate.
+        //Declaring my Consant Variables, this includes the Monthly insurance rate and the device amount insurance rate.
         public static decimal InsuranceRate = 0.9m, MonthlyRate = 0.95m;
-        //Declaring my Global variables. This includes my Insurance O
-        public static List<string> InsuredDevices = new List<string>();
-        public static decimal valueAfterInsured = 0.0m;
-        public static List<string> DeviceCategory = new List<string> { "Laptop", "Desktop", "Other" };
+        //Declaring my Global variables. This includes my Device category list
+        public static List<string> DEVICECATEGORY = new List<string> { "Laptop", "Desktop", "Other" };
+        //Insurance Summary global variables
+        public static int TotalDevices = 0, TotalLaptops = 0, TotalDesktops = 0, TotalOther = 0;
+        public static decimal TotalInsuranceValue = 0.0m, MostExpensive = 0, valueafterInsured = 0.0m;
+        //deviceName is global since ill be using it in my summary in my main method.
+        public static string deviceName = "";
 
 
 
 
         static void Main(string[] args)
         {
+            //Console Title
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(" /$$$$$$                                                                                       /$$$$$$                     \r\n|_  $$_/                                                                                      /$$__  $$                    \r\n  | $$   /$$$$$$$   /$$$$$$$ /$$   /$$  /$$$$$$  /$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$       | $$  \\ $$  /$$$$$$   /$$$$$$ \r\n  | $$  | $$__  $$ /$$_____/| $$  | $$ /$$__  $$|____  $$| $$__  $$ /$$_____/ /$$__  $$      | $$$$$$$$ /$$__  $$ /$$__  $$\r\n  | $$  | $$  \\ $$|  $$$$$$ | $$  | $$| $$  \\__/ /$$$$$$$| $$  \\ $$| $$      | $$$$$$$$      | $$__  $$| $$  \\ $$| $$  \\ $$\r\n  | $$  | $$  | $$ \\____  $$| $$  | $$| $$      /$$__  $$| $$  | $$| $$      | $$_____/      | $$  | $$| $$  | $$| $$  | $$\r\n /$$$$$$| $$  | $$ /$$$$$$$/|  $$$$$$/| $$     |  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$$      | $$  | $$| $$$$$$$/| $$$$$$$/\r\n|______/|__/  |__/|_______/  \\______/ |__/      \\_______/|__/  |__/ \\_______/ \\_______/      |__/  |__/| $$____/ | $$____/ \r\n                                                                                                       | $$      | $$      \r\n                                                                                                       | $$      | $$      \r\n                                                                                                       |__/      |__/      ");
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("!!This program is for educational use only!!");
+            
+            //While loop to continue for as much as the user needs
             char continueInput = 'y';
             while (continueInput == 'y' || continueInput.Equals('y'))
             {
+                
                 Console.WriteLine(OneDevice());
                 
 
                 Console.WriteLine("\n--------------------------------\nDo you want to process another device? (y/n)");
                 continueInput = Console.ReadLine()[0];
             }
-            
-
-
-
 
             //summary
+            Console.WriteLine("==============================Summary==============================\n\n");
+            Console.WriteLine($"Total Devices: {TotalDevices}\nTotal Laptops: {TotalLaptops}\nTotal Desktops: {TotalDesktops}\nTotal Other: {TotalOther}\n");
+            Console.WriteLine($"Most Expensive Device: {deviceName} @ {CalculateMostExpensive(valueafterInsured).ToString("C")}\n");
+            Console.WriteLine($"Total Insured Price of all Devices: {TotalInsuranceValue}");
         }
         //for my OneDevice I am limiting it to only user input.
        public static string OneDevice()
@@ -46,7 +53,6 @@ namespace AS92004
             
             string id = "";
             decimal price = 0;
-            string deviceName = "";
             int deviceAmount = 0;
             int devicecategoryNum = 0;
             string onedeviceOutput = "";
@@ -61,6 +67,8 @@ namespace AS92004
             //Capture Device amount
             Console.WriteLine("\nAmount of Devices: ");
             deviceAmount = (Convert.ToInt32(Console.ReadLine()));
+            //Gathering the total amount of devices
+            TotalDevices += deviceAmount;
 
             //Capture Device price
             Console.WriteLine("\nPrice per Device: ");
@@ -72,17 +80,33 @@ namespace AS92004
           
                 Console.WriteLine("\nWhat category is your Device?: \n(1. Laptop \n(2. Desktop \n(3. Other(Phones, Consoles, etc.)");
             devicecategoryNum = Convert.ToInt32(Console.ReadLine());
+            //Gathering the total Device Count
+           
 
-
-            //Generating and output for a Calculated Device
-            onedeviceOutput += "\n\n=====================Insurance Outcome=====================";
+            //Generating an output for a Calculated Device
+            onedeviceOutput += "\n\n=====================Insurance Outcome======================";
             onedeviceOutput += $"\nID: {id}\n";
             onedeviceOutput += $"\nName: {deviceName}";
-            onedeviceOutput += $"\nCategory: {DeviceCategory[devicecategoryNum-1]}";
+            onedeviceOutput += $"\nCategory: {DEVICECATEGORY[devicecategoryNum-1]}";
             onedeviceOutput += $"\nAmount: {deviceAmount}";
             onedeviceOutput += $"\nPrice: {price.ToString("C")}";
             onedeviceOutput += $"\nInsured Value: {CalculateValue(deviceAmount, price).ToString("C")}";
             onedeviceOutput += $"{CalculateMonthlyValue(deviceAmount, price)}";
+
+            //Putting it in my oneDevice gives me access to the local variables that are needed to be used, since its global it stores over the whole time instead of reseting after every loop.
+            //it also allows the summary count to run inside the while loop and calculate what is needed over time
+            if (devicecategoryNum == 1)
+            {
+                TotalLaptops += deviceAmount;
+            }
+            else if (devicecategoryNum == 2)
+            {
+                TotalDesktops += deviceAmount;
+            }
+            else
+            {
+                TotalOther += deviceAmount;
+            }
 
             
 
@@ -98,47 +122,63 @@ namespace AS92004
             return cost;
         }
 
-
         public static decimal CalculateValue(int deviceAmount, decimal price)
         {
             decimal valueafterInsured = 0.0m;
 
             if (deviceAmount > 5)
             {
-                valueafterInsured = ((CalculateCost(deviceAmount,price) - (5 * price)) * InsuranceRate);
+                valueafterInsured = ((CalculateCost(deviceAmount, price) - (5 * price)) * InsuranceRate);
                 valueafterInsured = valueafterInsured + (5 * price);
-
-
+               
             }
             else
             {
                 valueafterInsured = CalculateCost(deviceAmount, price);
+                
             }
-            
-           
+            //Running my most Expensive method every time a value is calculated.
+            CalculateMostExpensive(valueafterInsured);
+            //Also using calculate value to loop and add all the values altogether for the TotalInsuranceValue
+            TotalInsuranceValue += valueafterInsured;
+
             return valueafterInsured;
         }
         public static string CalculateMonthlyValue(int deviceAmount, decimal price)
         {
             decimal monthOne = 0, monthTwo = 0, monthThree = 0, monthFour = 0, monthFive = 0, monthSix = 0;
             string monthlyDepreciation = "";
-
+            
             monthOne = monthOne = CalculateValue(deviceAmount, price);
             monthTwo = monthOne * MonthlyRate;
             monthThree = monthTwo * MonthlyRate;
             monthFour = monthThree * MonthlyRate;
             monthFive = monthFour * MonthlyRate;
             monthSix = monthFive * MonthlyRate;
+
+            //Generating a summary for my Monthly summary.
             monthlyDepreciation += "\n========================Monthly Summary=====================";
-            monthlyDepreciation += $"\nMonth One: {monthOne.ToString("C")}";
-            monthlyDepreciation += $"\nMonth Two: {monthTwo.ToString("C")}";
-            monthlyDepreciation += $"\nMonth Three: {monthThree.ToString("C")}";
-            monthlyDepreciation += $"\nMonth Four: {monthFour.ToString("C")}";
-            monthlyDepreciation += $"\nMonth Five: {monthFive.ToString("C")}";
-            monthlyDepreciation += $"\nMonth Six: {monthSix.ToString("C")}";
+            monthlyDepreciation += $"\nMonth 1:      {monthOne.ToString("C")}";
+            monthlyDepreciation += $"\nMonth 2:      {monthTwo.ToString("C")}";
+            monthlyDepreciation += $"\nMonth 3:      {monthThree.ToString("C")}";
+            monthlyDepreciation += $"\nMonth 4:      {monthFour.ToString("C")}";
+            monthlyDepreciation += $"\nMonth 5:      {monthFive.ToString("C")}";
+            monthlyDepreciation += $"\nMonth 6:      {monthSix.ToString("C")}";
 
 
             return monthlyDepreciation;
+        }
+
+
+        public static decimal CalculateMostExpensive(decimal valueafterInsured)
+        {
+
+            if (valueafterInsured > MostExpensive)
+            { 
+            MostExpensive = valueafterInsured;
+            }
+
+            return MostExpensive;
         }
 
     }
